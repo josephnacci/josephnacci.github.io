38a83ba585ba665721839e75732638c7021dd7a4
Joseph Harder <josephharder@Nathans-MBP.fios-router.home>
2019-04-02 17:58:51 -0400

    subplot data

--- d3_script.js
+++ d3_script.js
@@ -86,8 +86,8 @@ console.log(nielsen);
 
       var prop = d.properties;
       var string = "<p><strong>Market Area Name</strong>: " + prop.dma1 + "</p>";
-      string += "<p><strong>Ratio of Female to Male gamers</strong>: " + prop.value.toFixed(2)*100 + "%</p>";
-      //string += "<p><strong>Nielsen Rank</strong>: " + prop.Rank + "</p>";
+      string += "<p><strong>Ratio of female to male gamers</strong>: " + prop.value.toFixed(2)*100 + "%</p>";
+      string += "<p><strong>Proportion of women who game</strong>: " + prop.prop_women_who_game.toFixed(2)*100 + "</p>";
 
       d3.select("#textbox")
         .html("")
